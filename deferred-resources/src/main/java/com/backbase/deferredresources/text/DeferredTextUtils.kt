a3ce20140da47012c22ff1f9e3b263612e2b0ff8
Drew Hamilton <drew.hamilton.0@gmail.com>
2020-06-22 22:21:18 +0200

    Add explicit return type to new extension function

--- deferred-resources/src/main/java/com/backbase/deferredresources/text/DeferredTextUtils.kt
+++ deferred-resources/src/main/java/com/backbase/deferredresources/text/DeferredTextUtils.kt
@@ -9,4 +9,4 @@ import com.backbase.deferredresources.DeferredText
  * Resolve a [DeferredText] to a string rather than a CharSequence by calling [toString] on the resolved value. Any
  * styling information from the original CharSequence will typically be removed from the resolved string.
  */
-fun DeferredText.resolveToString(context: Context) = resolve(context).toString()
+fun DeferredText.resolveToString(context: Context): String = resolve(context).toString()
