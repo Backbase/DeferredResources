622602867b4cf05c448517371ea3915eb98afa8e
Drew Hamilton <drew.hamilton@backbase.com>
2020-06-22 10:29:43 +0200

    Remove unwanted new interface function

--- deferred-resources/src/main/java/com/backbase/deferredresources/DeferredText.kt
+++ deferred-resources/src/main/java/com/backbase/deferredresources/DeferredText.kt
@@ -15,11 +15,6 @@ interface DeferredText {
     fun resolve(context: Context): CharSequence
 
     /**
-     * Resolve the text as a string.
-     */
-    fun resolveAsString(context: Context) = resolve(context).toString()
-
-    /**
      * A wrapper for a constant text [value].
      */
     @DataApi class Constant(
