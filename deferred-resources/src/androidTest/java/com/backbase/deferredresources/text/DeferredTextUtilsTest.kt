deed35a02731a98498413b7e23ae4c3a8c0d573f
Drew Hamilton <drew.hamilton.0@gmail.com>
2020-06-23 10:28:37 +0200

    Add DeferredText.resolveToString extension (#21)

---
+++ deferred-resources/src/androidTest/java/com/backbase/deferredresources/text/DeferredTextUtilsTest.kt
@@ -0,0 +1,25 @@
+package com.backbase.deferredresources.text
+
+import com.backbase.deferredresources.DeferredText
+import com.backbase.deferredresources.context
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class DeferredTextUtilsTest {
+
+    @Test fun resolveToString_returnsCharSequenceToString() {
+        val deferred = DeferredText.Constant(FakeCharSequence("Test string"))
+
+        assertThat(deferred.resolveToString(context)).isEqualTo(FakeCharSequence.TO_STRING)
+    }
+
+    private class FakeCharSequence(
+        private val wrapped: CharSequence
+    ) : CharSequence by wrapped {
+        override fun toString() = TO_STRING
+
+        companion object {
+            const val TO_STRING = "FakeCharSequence.toString"
+        }
+    }
+}
---
+++ deferred-resources/src/main/java/com/backbase/deferredresources/text/DeferredTextUtils.kt
@@ -0,0 +1,12 @@
+@file:JvmName("DeferredTextUtils")
+
+package com.backbase.deferredresources.text
+
+import android.content.Context
+import com.backbase.deferredresources.DeferredText
+
+/**
+ * Resolve a [DeferredText] to a string rather than a CharSequence by calling [toString] on the resolved value. Any
+ * styling information from the original CharSequence will typically be removed from the resolved string.
+ */
+fun DeferredText.resolveToString(context: Context): String = resolve(context).toString()
