dbfc43223a305e6510566fe2526e02330dc74773
Drew Hamilton <drew.hamilton.0@gmail.com>
2020-07-02 21:06:14 +0200

    Add ImageViewTest

---
+++ extensions/src/androidTest/java/com/backbase/deferredresources/extensions/ImageViewTest.kt
@@ -0,0 +1,50 @@
+package com.backbase.deferredresources.extensions
+
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.graphics.drawable.ColorDrawable
+import android.widget.ImageView
+import androidx.annotation.ColorInt
+import com.backbase.deferredresources.DeferredColor
+import com.backbase.deferredresources.drawable.asDrawable
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class ImageViewTest {
+
+    @Test fun setImageDrawable_setsResolvedDrawable() = onView<ImageView> {
+        val deferred = DeferredColor.Constant(Color.YELLOW).asDrawable()
+        setImageDrawable(deferred)
+
+        val imageDrawable = drawable as ColorDrawable
+        assertThat(imageDrawable.color).isEqualTo(Color.YELLOW)
+    }
+
+    @Test fun setColorFilter_withoutMode_setsColorInAtopMode() = onView<ImageView> {
+        val deferred = DeferredColor.Constant(Color.YELLOW)
+        setColorFilter(deferred)
+
+        val colorFilter = colorFilter as PorterDuffColorFilter
+        assertThat(colorFilter.getMode()).isEqualTo(PorterDuff.Mode.SRC_ATOP)
+        assertThat(colorFilter.getColor()).isEqualTo(Color.YELLOW)
+    }
+
+    @Test fun setColorFilter_withMode_setsColorInGivenMode() = onView<ImageView> {
+        val deferred = DeferredColor.Constant(Color.GREEN)
+        setColorFilter(deferred, PorterDuff.Mode.ADD)
+
+        val colorFilter = colorFilter as PorterDuffColorFilter
+        assertThat(colorFilter.getMode()).isEqualTo(PorterDuff.Mode.ADD)
+        assertThat(colorFilter.getColor()).isEqualTo(Color.GREEN)
+    }
+
+    private fun PorterDuffColorFilter.getMode(): PorterDuff.Mode {
+        return PorterDuffColorFilter::class.java.getDeclaredMethod("getMode").invoke(this) as PorterDuff.Mode
+    }
+
+    @ColorInt
+    private fun PorterDuffColorFilter.getColor(): Int {
+        return PorterDuffColorFilter::class.java.getDeclaredMethod("getColor").invoke(this) as Int
+    }
+}
