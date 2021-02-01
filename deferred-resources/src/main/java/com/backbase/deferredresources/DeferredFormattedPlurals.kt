e97521b994d659d0333aea24cc956f5fdee937e6
Drew Hamilton <drew.hamilton.0@gmail.com>
2021-02-01 14:20:55 +0100

    Refactor DeferredFormattedPlurals so @Parcelize works on API < 21

--- deferred-resources/src/main/java/com/backbase/deferredresources/DeferredFormattedPlurals.kt
+++ deferred-resources/src/main/java/com/backbase/deferredresources/DeferredFormattedPlurals.kt
@@ -24,20 +24,37 @@ public interface DeferredFormattedPlurals {
     /**
      * A wrapper for constant format-able pluralized text. [zero], [one], [two], [few], and [many] are locale-specific,
      * quantity-specific text values, while [other] is the fallback.
-     *
-     * [type] can be supplied to choose between [PluralRules.PluralType.CARDINAL] and [PluralRules.PluralType.ORDINAL].
-     * If null, the system default of [PluralRules.PluralType.CARDINAL] is used implicitly.
      */
+    // Primary constructor is internal rather than private so the generated Creator can access it
     @Parcelize
-    @DataApi public class Constant @RequiresApi(24) constructor(
+    @DataApi public class Constant internal constructor(
         private val other: String,
         private val zero: String = other,
         private val one: String = other,
         private val two: String = other,
         private val few: String = other,
         private val many: String = other,
-        private val type: PluralRules.PluralType? = null
+        // typeName is a String to allow @Parcelize to work as expected on API < 21. Directly using
+        //  PluralRules.PluralType, even if null, causes a VerifyError in the constructor at runtime on API < 21.
+        private val typeName: String?
     ) : ParcelableDeferredFormattedPlurals {
+
+        /**
+         * Constructor for API 24+. [type] can be supplied to choose between [PluralRules.PluralType.CARDINAL] and
+         * [PluralRules.PluralType.ORDINAL]. If null, the system default of [PluralRules.PluralType.CARDINAL] is used
+         * implicitly.
+         */
+        @RequiresApi(24)
+        public constructor(
+            other: String,
+            zero: String = other,
+            one: String = other,
+            two: String = other,
+            few: String = other,
+            many: String = other,
+            type: PluralRules.PluralType?
+        ) : this(other, zero, one, two, few, many, typeName = type?.name)
+
         /**
          * Constructor for API < 24. "CARDINAL" plural type will be used implicitly.
          */
@@ -49,18 +66,18 @@ public interface DeferredFormattedPlurals {
             two: String = other,
             few: String = other,
             many: String = other
-        ) : this(other, zero, one, two, few, many, null)
+        ) : this(other, zero, one, two, few, many, typeName = null)
 
         /**
          * Resolves and formats one of [zero], [one], [two], [few], [many], or [other] depending on the primary locale
          * from [context], the given [quantity], and [formatArgs].
          */
-        @SuppressLint("NewApi") // `type` is known to be safely null on API < 24
         override fun resolve(context: Context, quantity: Int, vararg formatArgs: Any): String {
-            val pluralRules = if (type == null)
+            val pluralRules = if (typeName == null)
                 PluralRulesCompat.forContext(context)
             else
-                PluralRulesCompat.forContext(context, type)
+                @Suppress("NewApi") // If type is non-null, we're on API 24+
+                PluralRulesCompat.forContext(context, PluralRules.PluralType.valueOf(typeName))
 
             val unformatted = when (pluralRules.select(quantity)) {
                 PluralRulesCompat.KEYWORD_ZERO -> zero
