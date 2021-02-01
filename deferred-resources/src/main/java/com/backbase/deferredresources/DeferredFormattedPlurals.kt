a9d598f6047408c93268b35892e4bee38367fe0d
Drew Hamilton <drew.hamilton.0@gmail.com>
2021-02-01 15:34:13 +0100

    Refactor DeferredFormattedPlurals to reduce duplication

--- deferred-resources/src/main/java/com/backbase/deferredresources/DeferredFormattedPlurals.kt
+++ deferred-resources/src/main/java/com/backbase/deferredresources/DeferredFormattedPlurals.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.icu.text.PluralRules
 import androidx.annotation.PluralsRes
 import androidx.annotation.RequiresApi
-import com.backbase.deferredresources.internal.PluralRulesCompat
 import com.backbase.deferredresources.internal.primaryLocale
 import com.backbase.deferredresources.text.ParcelableDeferredFormattedPlurals
 import dev.drewhamilton.extracare.DataApi
@@ -22,21 +21,12 @@ public interface DeferredFormattedPlurals {
     public fun resolve(context: Context, quantity: Int, vararg formatArgs: Any = arrayOf(quantity)): String
 
     /**
-     * A wrapper for constant format-able pluralized text. [zero], [one], [two], [few], and [many] are locale-specific,
-     * quantity-specific text values, while [other] is the fallback.
+     * A wrapper for constant format-able pluralized text.
      */
     // Primary constructor is internal rather than private so the generated Creator can access it
     @Parcelize
     @DataApi public class Constant internal constructor(
-        private val other: String,
-        private val zero: String = other,
-        private val one: String = other,
-        private val two: String = other,
-        private val few: String = other,
-        private val many: String = other,
-        // typeName is a String to allow @Parcelize to work as expected on API < 21. Directly using
-        //  PluralRules.PluralType, even if null, causes a VerifyError in the constructor at runtime on API < 21.
-        private val typeName: String?
+        private val delegate: DeferredPlurals.Constant
     ) : ParcelableDeferredFormattedPlurals {
 
         /**
@@ -53,7 +43,7 @@ public interface DeferredFormattedPlurals {
             few: String = other,
             many: String = other,
             type: PluralRules.PluralType?
-        ) : this(other, zero, one, two, few, many, typeName = type?.name)
+        ) : this(delegate = DeferredPlurals.Constant(other, zero, one, two, few, many, type))
 
         /**
          * Constructor for API < 24. "CARDINAL" plural type will be used implicitly.
@@ -66,27 +56,14 @@ public interface DeferredFormattedPlurals {
             two: String = other,
             few: String = other,
             many: String = other
-        ) : this(other, zero, one, two, few, many, typeName = null)
+        ) : this(delegate = DeferredPlurals.Constant(other, zero, one, two, few, many))
 
         /**
-         * Resolves and formats one of [zero], [one], [two], [few], [many], or [other] depending on the primary locale
+         * Resolves and formats one of the constructor-provided pluralized values depending on the primary locale
          * from [context], the given [quantity], and [formatArgs].
          */
         override fun resolve(context: Context, quantity: Int, vararg formatArgs: Any): String {
-            val pluralRules = if (typeName == null)
-                PluralRulesCompat.forContext(context)
-            else
-                @Suppress("NewApi") // If type is non-null, we're on API 24+
-                PluralRulesCompat.forContext(context, PluralRules.PluralType.valueOf(typeName))
-
-            val unformatted = when (pluralRules.select(quantity)) {
-                PluralRulesCompat.KEYWORD_ZERO -> zero
-                PluralRulesCompat.KEYWORD_ONE -> one
-                PluralRulesCompat.KEYWORD_TWO -> two
-                PluralRulesCompat.KEYWORD_FEW -> few
-                PluralRulesCompat.KEYWORD_MANY -> many
-                else -> other
-            }
+            val unformatted = delegate.resolve(context, quantity).toString()
             return String.format(context.primaryLocale, unformatted, *formatArgs)
         }
     }
