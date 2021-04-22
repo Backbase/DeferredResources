57c96e1d41bf878698a78abff558647b0fa29257
Drew Hamilton <github@drewhamilton.dev>
2021-04-22 09:50:19 +0200

    Compose 1.0.0-beta05

--- deferred-resources-compose/src/main/java/com/backbase/deferredresources/compose/DeferredDrawable.kt
+++ deferred-resources-compose/src/main/java/com/backbase/deferredresources/compose/DeferredDrawable.kt
@@ -1,20 +1,13 @@
 package com.backbase.deferredresources.compose
 
-import android.graphics.drawable.Animatable
-import android.graphics.drawable.Drawable
-import android.os.Handler
-import android.os.Looper
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import com.backbase.deferredresources.DeferredDrawable
-import com.google.accompanist.imageloading.AndroidDrawablePainter
-import com.google.accompanist.imageloading.toPainter
+import com.google.accompanist.imageloading.rememberDrawablePainter
 
 /**
  * Resolve a [deferredDrawable] into a Compose [Painter], remembering the resulting painter as long as the current
@@ -27,50 +20,8 @@ import com.google.accompanist.imageloading.toPainter
         deferredDrawable.resolve(context)
     }
 
-    // region TODO: Replace with Accompanist `rememberDrawablePainter` when available
-    val painter = remember(drawable) {
-        drawable?.toPainter() ?: ColorPainter(Color.Transparent)
+    return when (drawable) {
+        null -> remember { ColorPainter(Color.Transparent) }
+        else -> rememberDrawablePainter(drawable)
     }
-
-    val invalidateTick = remember {
-        mutableStateOf(0)
-    }
-    val callback = object : Drawable.Callback {
-        override fun invalidateDrawable(d: Drawable) {
-            // Update the tick so that we get re-drawn
-            invalidateTick.value++
-        }
-
-        override fun scheduleDrawable(d: Drawable, what: Runnable, time: Long) {
-            MAIN_HANDLER.postAtTime(what, time)
-        }
-
-        override fun unscheduleDrawable(d: Drawable, what: Runnable) {
-            MAIN_HANDLER.removeCallbacks(what)
-        }
-    }
-
-    DisposableEffect(painter) {
-        if (painter is AndroidDrawablePainter) {
-            checkNotNull(drawable)
-            drawable.callback = callback
-            drawable.setVisible(true, true)
-            if (drawable is Animatable) drawable.start()
-        }
-        onDispose {
-            if (painter is AndroidDrawablePainter) {
-                checkNotNull(drawable)
-                if (drawable is Animatable) drawable.stop()
-                drawable.setVisible(false, false)
-                drawable.callback = null
-            }
-        }
-    }
-
-    return painter
-    //endregion
-}
-
-private val MAIN_HANDLER by lazy(LazyThreadSafetyMode.NONE) {
-    Handler(Looper.getMainLooper())
 }
